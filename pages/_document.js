b6bc1e8a0913174173edfbc234549f0ef712619c
ComBba <cent@naver.com>
2023-07-24 15:38:25 +0900

    meta description 변경

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="KMED Tours Chat with AI offers a safe and efficient way to connect patients and doctors." />
+        <meta name="description" content="KMED Tours Chat with AI provides a new way to tour while using medical examinations, oriental clinics, and dental clinics in Korea." />
         <meta property="og:title" content="KMED Tours Chat with AI" />
-        <meta property="og:description" content="A safe and efficient way to connect patients and doctors." />
+        <meta property="og:description" content="A new way to tour while using medical examinations, oriental clinics, and dental clinics in Korea." />
         <meta property="og:image" content="https://kmed-tours-chat-service.vercel.app/kmed-logo.png" />
         <meta property="og:url" content="https://kmed-tours-chat-service.vercel.app/" />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="https://kmed-tours-chat-service.vercel.app/kmed-logo.png" />
+        <meta name="twitter:title" content="KMED Tours Chat with AI" />
+        <meta name="twitter:description" content="A new way to tour while using medical examinations, oriental clinics, and dental clinics in Korea." />
+        <title>KMED Tours Chat with AI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
